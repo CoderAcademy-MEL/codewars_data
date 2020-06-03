0c7e8703eb7f5db53b6e50800f9db019a5b472ba
Edward Deam <edwardcrdeam@gmail.com>
2020-06-03 02:43:42 -0700

    refactoring method names / remove unneeded variables

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const readlineSync = require('readline-sync');
 
 const requestUserData = async (username) => {
   console.log(`Searching for ${username} on CodeWars...`);
+
   const BASE_URL = 'https://www.codewars.com/api/v1/';
   const response = await fetch(`${BASE_URL}/users/${username}`);
   const data = await response.json();
@@ -10,16 +11,13 @@ const requestUserData = async (username) => {
   return data;
 };
 
-const getUserInput = () => {
-  const username = readlineSync.question('Please Enter a Username: ');
-  return username;
-};
+const getUserInput = () => readlineSync.question('Please Enter a Username: ');
 
 const getMenuOption = () => readlineSync.questionInt('> ');
 
 const waitForKeyPress = () => readlineSync.keyIn('Press any key to continue...');
 
-const menu = (username) => {
+const displayMenu = (username) => {
   console.log(`CodeWars User: ${username}`);
   console.log('1. Stats');
   console.log('2. Languages');
@@ -32,6 +30,7 @@ const displayUserStats = (data) => {
   console.log(`Leaderboard Position: #${data.leaderboardPosition}`);
   console.log(`Rank Score: ${data.ranks.overall.score}`);
   console.log(`Rank: ${data.ranks.overall.name}`);
+
   waitForKeyPress();
 };
 
@@ -45,19 +44,22 @@ const processLanguageData = (languages) => Object.keys(languages).map((key) => (
 const displayLanguageStats = (languageObj) => {
   const languages = processLanguageData(languageObj);
   languages.forEach((language) => {
-    const { lang, name, color, score } = language;
+    const {
+      lang, name, color, score,
+    } = language;
     console.log(`${lang}: has the ${color} rank of ${name} with ${score} points!`);
   });
+
   waitForKeyPress();
 };
 
 const app = async () => {
   const username = getUserInput();
   const data = await requestUserData(username);
-  let running = true;
 
+  let running = true;
   while (running) {
-    menu(data.username);
+    displayMenu(data.username);
     const choice = getMenuOption();
 
     switch (choice) {
